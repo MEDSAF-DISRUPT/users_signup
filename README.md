f5e65f3fe47c51746282959cc7964c4512a004e4
Ewetoye Ibrahim <ibrahim@medsaf.com>
2020-04-23 17:10:09 +0100

    first trial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Users Signup 
+It extends Odoo's default auth_signup module to add more field into
+the sign up form presented to new users.
---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Users Signup Form',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'sequence': 1,
+    'author': 'Ewetoye, Ibrahim',
+    'website': 'Medsaf.com',
+    'depends': ['auth_signup'],
+    'data': [
+        'views/auth_signup_extend_views.xml',
+        'views/res_partner_view.xml',
+    ],
+    'qweb': [],
+    'css': [],
+    'js': [],
+    'images': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3'
+}
